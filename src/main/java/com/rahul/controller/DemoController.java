47a59c3084fb992d42032a7cd687d778dddb7c36
Ayushi Gupta <ayushi7453gupta@gmail.com>
2024-12-24 21:26:20 +0530

    fetched course data

--- src/main/java/com/rahul/controller/DemoController.java
+++ src/main/java/com/rahul/controller/DemoController.java
@@ -1,50 +1,65 @@
 package com.rahul.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.ui.Model;
+import com.rahul.model.Course;
+import com.rahul.service.CourseService;
+import java.util.List;
 
 @Controller
 public class DemoController {
-	
-	@GetMapping({"/","/index"})
+	@Autowired
+    private CourseService service;
+    @Autowired
+    private CourseService courseService;
+	@GetMapping({ "/", "/index" })
 	public String Home() {
-		return "index";		
+		return "index";
 	}
 	@GetMapping("/courses")
-	public String courses() {
-		return "courses";		
+	public String showCourses(Model model) {
+		List<Course> pendingCourses = courseService.getPendingCourse();
+		List<Course> approvedCourses = courseService.getApprovedCourses();
+		List<Course> rejectedCourses = courseService.getRejectedCourses();
+	
+		model.addAttribute("pendingCourses", pendingCourses);
+		model.addAttribute("approvedCourses", approvedCourses);
+		model.addAttribute("rejectedCourses", rejectedCourses);
+	
+		return "courses"; // Name of the Thymeleaf template
 	}
+
 	@GetMapping("/pricing")
 	public String pricing() {
-		return "pricing";		
+		return "pricing";
 	}
+
 	@GetMapping("/about")
 	public String about() {
-		return "about";		
+		return "about";
 	}
+
 	@GetMapping("/events")
 	public String events() {
-		return "events";		
+		return "events";
 	}
+
 	@GetMapping("/trainers")
 	public String trainers() {
-		return "trainers";		
+		return "trainers";
 	}
-	
+
 	@GetMapping("/contact")
 	public String contact() {
-		return "contact";		
+		return "contact";
 	}
+
 	@GetMapping("/course-details")
-    public String coursedetails() {
-        return "course-details"; // This should resolve to course-details.html in templates
-    }
+	public String coursedetails() {
+		return "course-details"; // This should resolve to course-details.html in templates
+	}
 
-	
-	
-
-	
 }
