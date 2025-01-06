f18b55c17886c431151d65892c30df9a8c80ea4f
codewithrahuldixit <140512374+codewithrahuldixit@users.noreply.github.com>
2025-01-06 16:57:28 +0530

    Rahul Changes

--- src/main/java/com/rahul/service/TrainerService.java
+++ src/main/java/com/rahul/service/TrainerService.java
@@ -53,7 +53,7 @@ public class TrainerService {
         }
     
         // Directory where the image will be stored on the server
-        String uploadDir = "C:/Users/isp/Documents/ayushi_pagal/TeachToTech/src/main/resources/static/assets/img/team/";
+        String uploadDir = "D:/TeachToTech/TeachToTech/src/main/resources/static/assets/img/team/";
         String fileName = imageFile.getOriginalFilename();
     
         try {
