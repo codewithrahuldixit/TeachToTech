68d87b626146be600ef80613dbf9357cff29651e
codewithrahuldixit <140512374+codewithrahuldixit@users.noreply.github.com>
2025-02-13 22:42:14 +0530

    Changes 13:02:2024

--- src/main/java/com/rahul/TeachToTechApplication.java
+++ src/main/java/com/rahul/TeachToTechApplication.java
@@ -2,14 +2,11 @@ package com.rahul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.rahul.model")  // ✅ Ensures Hibernate scans the right package
-@EnableJpaRepositories("com.rahul.repository") // ✅ Ensures repositories are found
-@ComponentScan(basePackages = "com.rahul")
+//@EntityScan("com.rahul.model")  // ✅ Ensures Hibernate scans the right package
+//@EnableJpaRepositories("com.rahul.repository") // ✅ Ensures repositories are found
+//@ComponentScan(basePackages = "com.rahul")
 public class TeachToTechApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rahul/controller/ArticleController.java
+++ src/main/java/com/rahul/controller/ArticleController.java
@@ -1,17 +1,14 @@
 package com.rahul.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 
 import com.rahul.model.Article;
 import com.rahul.model.Category;
@@ -19,9 +16,6 @@ import com.rahul.repository.CategoryRepository;
 import com.rahul.service.ArticleService;
 
 import jakarta.servlet.http.HttpSession;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
